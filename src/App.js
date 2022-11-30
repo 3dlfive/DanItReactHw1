244a6628e23c33e1bea31637d0ed3d4728a6affd
3lfive <denystry@gmail.com>
2022-11-30 13:32:35 -0500

    feat: close outside

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import Button from './components/Button/button.component'
 import Modal from './components/Modal/modal.component'
 
 class App extends Component {
-  state = {isModal:false}
-  handleClick = () =>{
-    console.log("Hi there, user!");
-  }
+  state = {isModal:false,modalHeader:"Modal window one",modalCloseButton:true,modalText:"Modal text one"}
+  handleClick = (e) =>{
+    console.log(this.text);
+
+    this.setState((prevstate)=>
+     {return {
+      ...prevstate,
+      isModal: !prevstate.isModal}})
+    }
+
   render(){
-    const {isModal}= this.state
+    const {isModal,modalHeader,modalCloseButton,modalText}= this.state
 
 
 
     return (
       <div>
         <p>Test Test</p>
-        <Button text="Open first modal" onClick={this.handleClick} BGcolor={{ backgroundColor: '#01FF70' }} />
-        <Button text="Open second modal" onClick={this.handleClick} BGcolor={{ backgroundColor: '#F012BE' }} />
-        {isModal && <Modal />}
+        <Button text="Open first modal" onClick={this.handleClick} BGcolor={{ backgroundColor: '#01FF70' }} isModal={isModal} />
+        <Button text="Open second modal" onClick={this.handleClick} BGcolor={{ backgroundColor: '#F012BE' }} isModal={isModal}/>
+        {isModal && <Modal header={modalHeader} closeButton={this.handleClick}/>}
       </div>
     );
   }
--- src/components/Button/button.component.jsx
+++ src/components/Button/button.component.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import './button.styles.scss'
+// import Modal from '../Modal/modal.component'
 
 class Button extends Component {
+
   render(){
-    const {text,BGcolor,onClick} = this.props;
-    return (<button type="button" onClick={onClick} style={BGcolor} className="button-15" >{text}</button>)
+    const {text,BGcolor,onClick,isModal,modalHeader} = this.props;
+    return (<button type="button"  onClick={onClick} style={BGcolor} className="button-15" >{text}</button>)
+
   }
 }
 
