675bad48ffe411bb2d62ffd4cae54c928dc75a92
3lfive <denystry@gmail.com>
2023-01-20 16:03:36 -0500

    fix: remove logger package

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
 import {compose, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+
 import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 
 
 import { rootReducer } from './root-reducer'
 
-const middleWares = [logger,thunk]
+const middleWares = [thunk]
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
